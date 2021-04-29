1198e7d463b7fc9154058a62d0ee6b10da30e46c
cat-bro <cathjb@gmail.com>
2021-04-29 14:16:03 +1000

    .

--- query-jobs-tool-and-stderr.sh
+++ query-jobs-tool-and-stderr.sh
@@ -28,3 +28,36 @@ local_query-jobs-tool-and-stderr() { ## input tool substr,  stderr substr, optio
 			LIMIT $limit
 	EOF
 }
+
+local_query-jobs-tool-has-stderr() { ## input tool substr,  stderr substr, optional limit
+    tool_substr="$1"
+	tool_stderr_substr="$2"
+    limit="$3"
+	[ ! "$3" ] && limit="50" || limit="$3"
+	handle_help "$@" <<-EOF
+	EOF
+
+	read -r -d '' QUERY <<-EOF
+			SELECT
+				j.id as job_id,
+				j.create_time as created,
+				j.state as state,
+				j.tool_id as tool_id,
+				(
+					SELECT
+					pg_size_pretty(SUM(d.total_size))
+					FROM dataset d, history_dataset_association hda, job_to_input_dataset jtid
+					WHERE hda.dataset_id = d.id
+					AND jtid.job_id = j.id
+					AND hda.id = jtid.dataset_id
+				) as sum_input_size,
+				(
+					position('$tool_stderr_substr' in j.tool_stderr)>0
+				) as has_stderr,
+				j.destination_id as destination
+			FROM job j
+            WHERE position('$tool_substr' in j.tool_id)>0
+			ORDER BY j.create_time desc
+			LIMIT $limit
+	EOF
+}
