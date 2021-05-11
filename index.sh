d32c5557a3057bb6257930ca7cb0dd4b6bea6b8e
cat-bro <cathjb@gmail.com>
2021-05-12 09:34:27 +1000

    add modified query-training-members

--- index.sh
+++ index.sh
@@ -10,6 +10,7 @@ source $HERE/query-jobs-by-tool-stdout.sh
 source $HERE/query-time-size-by-tool.sh
 source $HERE/query-jobs-tool-and-stderr.sh
 source $HERE/query-new-job-sizes.sh
+source $HERE/query-training-members.sh
 
 source $HERE/job_errors/query-job-info.sh
 source $HERE/job_errors/query-job-stdout.sh
---
+++ query-training-members.sh
@@ -0,0 +1,49 @@
+HERE=${BASH_SOURCE%/*}
+
+# source $HERE/functions/gdpr_safe.sh
+
+local_query-training-members() { ##? <tr_id>: List users in a specific training
+	handle_help "$@" <<-EOF
+		    $ gxadmin query training-members hts2018
+		          username      |       joined
+		    --------------------+---------------------
+		     helena-rasche      | 2018-09-21 21:42:01
+	EOF
+
+	# Remove training- if they used it.
+	ww=$(echo "$arg_tr_id" | sed 's/^training-//g')
+	username=$(gdpr_safe galaxy_user.username username)
+
+	read -r -d '' QUERY <<-EOF
+			SELECT ($username) as username,
+				date_trunc('second', user_group_association.create_time AT TIME ZONE 'UTC') as joined
+			FROM galaxy_user, user_group_association, galaxy_group
+			WHERE galaxy_group.name = 'training-$ww'
+				AND galaxy_group.id = user_group_association.group_id
+				AND user_group_association.user_id = galaxy_user.id
+	EOF
+}
+
+
+
+# local_query-training-members() { ##? <tr_id>: List users in a specific training
+# 	handle_help "$@" <<-EOF
+# 		    $ gxadmin query training-members hts2018
+# 		          username      |       joined
+# 		    --------------------+---------------------
+# 		     helena-rasche      | 2018-09-21 21:42:01
+# 	EOF
+
+# 	# Remove training- if they used it.
+# 	ww=$(echo "$arg_tr_id" | sed 's/^training-//g')
+# 	username=$(gdpr_safe galaxy_user.username username)
+
+# 	read -r -d '' QUERY <<-EOF
+# 			SELECT COALESCE(galaxy_user.username::text, '__UNKNOWN__') as username,
+# 				date_trunc('second', user_group_association.create_time AT TIME ZONE 'UTC') as joined
+# 			FROM galaxy_user, user_group_association, galaxy_group
+# 			WHERE galaxy_group.name = 'training-$ww'
+# 				AND galaxy_group.id = user_group_association.group_id
+# 				AND user_group_association.user_id = galaxy_user.id
+# 	EOF
+# }
