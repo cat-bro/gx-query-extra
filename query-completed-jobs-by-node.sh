49d65e84c789f854ac1a2243c8e1de00db6dbeb7
cat-bro <cathjb@gmail.com>
2021-09-10 17:55:52 +1000

    .

--- query-completed-jobs-by-node.sh
+++ query-completed-jobs-by-node.sh
@@ -20,7 +20,7 @@ local_query-completed-jobs-by-node() { ## input limit,  # optional string of mor
 			AND jmt.job_id = j.id
 			AND jmt.metric_name = 'HOSTNAME'
             AND j.state in ('ok', 'deleted', 'error')
-            AND jmt.metric_value = $host_name
+            AND jmt.metric_value = '$host_name'
 			ORDER BY j.update_time desc
 			LIMIT $limit
 	EOF
