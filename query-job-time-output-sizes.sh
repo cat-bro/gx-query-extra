6d5ba813232b6424de39fe468ce7fe468e90e871
cat-bro <cathjb@gmail.com>
2021-09-13 12:26:52 +1000

    .

--- query-job-time-output-sizes.sh
+++ query-job-time-output-sizes.sh
@@ -13,12 +13,13 @@ local_query-job-time-output-sizes() { ##? <tool> input tool substr,  # optional 
 				j.tool_id as tool_id,
                 d.total_size as dataset_size,
                 j.destination_id as destination_id
-			FROM job_to_output_dataset jtod, dataset d, job j, galaxy_user u
+			FROM job_to_output_dataset jtod, dataset d, job j, galaxy_user u, history_dataset_association hda
 			WHERE j.user_id = u.id
             AND jtod.job_id = j.id
             AND j.create_time < '$datetime'
 			AND j.update_time > '$datetime'
-			AND jtod.dataset_id = d.id
+			AND jtod.dataset_id = hda.id
+			AND hda.dataset_id = d.id
 			ORDER BY j.create_time desc
 
 	EOF
