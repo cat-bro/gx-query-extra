ea8cdb0ca1ef69accd5c213d8bba4ada53d0c9f9
cat-bro <cathjb@gmail.com>
2021-09-14 18:23:10 +1000

    .

--- query-1slot-jobs-by-walltime.sh
+++ query-1slot-jobs-by-walltime.sh
@@ -3,6 +3,21 @@ local_query-1slot-jobs-by-walltime() { ## input seconds,  # optional limit
 	[ ! "$2" ] && limit="50" || limit="$2"
 
 	handle_help "$@" <<-EOF
+
+    Produces a table of completed jobs with running time *greater than* the input argument in seconds that have
+    run on the slurm_1slot destination.  There is an optional second argument of the number of rows to return
+    (default 50).
+
+    For example, the last 4 jobs that ran on slurm_1slot for more than half an hour (1800 seconds)
+
+    gxadmin local query-1slot-jobs-by-walltime 1800 10
+     job_id  |        update_time         |                                                   tool_id                                                    | runtime  | sum_input_size
+    ---------+----------------------------+--------------------------------------------------------------------------------------------------------------+----------+----------------
+     7535177 | 2021-09-14 06:48:04.311428 | toolshed.g2.bx.psu.edu/repos/devteam/fastq_paired_end_interlacer/fastq_paired_end_interlacer/1.2.0.1+galaxy0 | 00:38:25 | 603 MB
+     7534756 | 2021-09-14 04:17:04.111619 | toolshed.g2.bx.psu.edu/repos/iuc/scanpy_remove_confounders/scanpy_remove_confounders/1.7.1+galaxy0           | 00:56:32 | 4659 MB
+     7534276 | 2021-09-14 03:17:39.812616 | toolshed.g2.bx.psu.edu/repos/iuc/scanpy_remove_confounders/scanpy_remove_confounders/1.7.1+galaxy0           | 01:14:35 | 4659 MB
+     7534370 | 2021-09-14 03:14:51.898115 | toolshed.g2.bx.psu.edu/repos/iuc/fastqe/fastqe/0.2.6+galaxy0                                                 | 01:19:38 | 9799 MB
+
 	EOF
 
 	read -r -d '' QUERY <<-EOF
--- query-walltime-size-by-tool.sh
+++ query-walltime-size-by-tool.sh
@@ -2,8 +2,20 @@ local_query-walltime-size-by-tool() { ##? <tool> input tool substr,  # optional 
 	tool_substr="$1"
 	[ ! "$2" ] && limit="10" || limit="$2"
 	handle_help "$@" <<-EOF
-	EOF
 
+    Produces a table with input size and running times for a given tool, provided as the first argument.
+    The argument is a substring of the tool_id e.g. `trinity`, `trinity/2.9.1+galaxy1`, `Count1`.  An
+    optional second argument is the number of rows to return (default 50).
+
+    gxadmin local query-walltime-size-by-tool busco 5
+     job_id  |          created           |          updated           |   username    |  state  |                          tool_id                           | runtime  | sum_input_size | destination
+    ---------+----------------------------+----------------------------+---------------+---------+------------------------------------------------------------+----------+----------------+--------------
+     7535204 | 2021-09-14 06:13:44.820678 | 2021-09-14 07:27:47.494749 | julia         | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.0.0+galaxy0 | 01:13:59 | 148 MB         | slurm_3slots
+     7533738 | 2021-09-13 14:10:02.462621 | 2021-09-13 18:06:15.366452 | paul          | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 03:56:10 | 31 MB          | slurm_1slot
+     7533781 | 2021-09-13 14:53:51.879839 | 2021-09-13 14:54:24.656188 | kevin         | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 00:00:31 | 2244 kB        | slurm_1slot
+     7533323 | 2021-09-13 08:15:12.673086 | 2021-09-13 12:10:33.847549 | paul          | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 03:55:18 | 31 MB          | slurm_1slot
+     7533213 | 2021-09-13 05:59:59.559287 | 2021-09-13 07:21:51.458181 | paul          | deleted | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 |          | 3970 MB        | slurm_3slots
+	EOF
 
 	read -r -d '' QUERY <<-EOF
 			SELECT
