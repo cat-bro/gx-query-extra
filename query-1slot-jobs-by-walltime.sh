7d537c3507bf7acdb709b876b023066a72b37684
cat-bro <cathjb@gmail.com>
2021-09-14 17:53:31 +1000

    .

--- query-1slot-jobs-by-walltime.sh
+++ query-1slot-jobs-by-walltime.sh
@@ -9,7 +9,6 @@ local_query-1slot-jobs-by-walltime() { ## input seconds,  # optional limit
         SELECT
             jmn.job_id as job_id,
             j.tool_id as tool_id,
-            jmn.metric_value as runtime_seconds,
             TO_CHAR((jmn.metric_value || ' second')::interval, 'HH24:MI:SS') as runtime,
             (
                 SELECT
