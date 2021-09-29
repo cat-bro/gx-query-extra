e55081849d3b9b72c2080a8326ae72140dc1fea7
cat-bro <cathjb@gmail.com>
2021-09-29 15:38:19 +1000

    .

--- query-job-output-datasets.sh
+++ query-job-output-datasets.sh
@@ -1,15 +1,17 @@
 local_query-job-output-datasets() { ##? <id>: Job ID
-	# similar to gxadmin query-job-inputs but with sizes
+	# similar to gxadmin query-job-inputs but for outputs including file sizes
 	arg_id="$1"
 	handle_help "$@" <<-EOF
 	Shows a table of job output datasets with file_size and total size for a given job id
 	
-	$ gxadmin local query-job-output-datasets 4092986755
-
-	 hda_id  |            hda_name             |  d_id   | d_state | d_file_size | d_total_size
-	---------+---------------------------------+---------+---------+-------------+--------------
-	91237625 | SRR7692603:forward uncompressed | 3545195 | ok      | 4810 MB     | 4810 MB
-	91237627 | SRR7692603:reverse uncompressed | 3545196 | ok      | 4845 MB     | 4845 MB
+	$ gxadmin local query-job-output-datasets 2996484
+	 hda_id  |                  hda_name                  | extension |  d_id   | d_state | d_file_size | d_total_size
+	---------+--------------------------------------------+-----------+---------+---------+-------------+--------------
+	 6073903 | TopHat on data  TY5_N 39: align_summary    | txt       | 5301465 | ok      | 203 bytes   | 203 bytes
+	 6073904 | TopHat on data  TY5_N 39: insertions       | bed       | 5301466 | ok      | 6458 kB     | 6458 kB
+	 6073905 | TopHat on data  TY5_N 39: deletions        | bed       | 5301467 | ok      | 5760 kB     | 5760 kB
+	 6073906 | TopHat on data  TY5_N 39: splice junctions | bed       | 5301468 | ok      | 3589 kB     | 3589 kB
+	 6073907 | TopHat on data TY5_N 39: accepted_hits     | bam       | 5301469 | ok      | 1982 MB     | 1982 MB
 
 	EOF
 
--- query-jobs-running-at-datetime.sh
+++ query-jobs-running-at-datetime.sh
@@ -1,6 +1,18 @@
-local_query-jobs-running-at-datetime() {
+local_query-jobs-running-at-datetime() { ##? <datetime> datetime string
 	datetime="$1"
 	handle_help "$@" <<-EOF
+
+    Find all jobs in queued, running or ok state that were created before and updated after a given datetime.  This was written with the aim of finding
+    non-deleted jobs that may have two sets of data files due to the hierarchical object store path having changed while they were running.
+
+    $ gxadmin local query-jobs-running-at-datetime "2021-09-28 14:00"
+     job_id  |          created           |          updated           | user_id | state |                                   tool_id                                   | destination_id
+    ---------+----------------------------+----------------------------+---------+-------+-----------------------------------------------------------------------------+-----------------
+     7396484 | 2021-09-28 13:59:25.91883  | 2021-09-28 15:09:09.148783 |   24601 | ok    | toolshed.g2.bx.psu.edu/repos/devteam/tophat2/tophat2/2.1.1                  | pulsar-mel3_mid
+     7396483 | 2021-09-28 13:58:59.578761 | 2021-09-28 15:06:50.605129 |   24601 | ok    | toolshed.g2.bx.psu.edu/repos/devteam/tophat2/tophat2/2.1.1                  | slurm_1slot
+     7396455 | 2021-09-28 13:27:11.979348 | 2021-09-28 14:01:13.616717 |    2468 | ok    | toolshed.g2.bx.psu.edu/repos/devteam/fastqc/fastqc/0.73+galaxy0             | pulsar-mel3_mid
+     7396432 | 2021-09-28 12:59:47.650807 | 2021-09-28 14:49:57.488824 |    2468 | ok    | toolshed.g2.bx.psu.edu/repos/pjbriggs/trimmomatic/trimmomatic/0.36.6        | slurm_3slots
+     7396349 | 2021-09-28 12:35:23.974676 | 2021-09-28 14:32:15.411004 |   21583 | ok    | toolshed.g2.bx.psu.edu/repos/iuc/unicycler/unicycler/0.4.8.0                | pulsar-mel3_mid
 	EOF
 
 	read -r -d '' QUERY <<-EOF
