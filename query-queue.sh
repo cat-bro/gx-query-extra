66a97f53da2810c82b4e31a664954bbf79cdb748
cat-bro <cathjb@gmail.com>
2021-11-28 18:45:18 +1100

    .

--- query-queue.sh
+++ query-queue.sh
@@ -1,20 +1,13 @@
 local_query-queue() { ##? [--all] [--seconds] [--since-update]: Detailed overview of running and queued jobs
-    # this is a rip off of gxadmin query queue-detail with job destination info (cores/mem)
+	# this is a copy of gxadmin query queue-detail with job destination info (cores/mem/partition) added and runner_id, count removed
 	handle_help "$@" <<-EOF
-		    $ gxadmin query queue-detail
-		      state  |   id    |  extid  |                                 tool_id                                   | username | time_since_creation
-		    ---------+---------+---------+---------------------------------------------------------------------------+----------+---------------------
-		     running | 4360629 | 229333  | toolshed.g2.bx.psu.edu/repos/bgruening/infernal/infernal_cmsearch/1.1.2.0 | xxxx     | 5 days 11:00:00
-		     running | 4362676 | 230237  | toolshed.g2.bx.psu.edu/repos/iuc/mothur_venn/mothur_venn/1.36.1.0         | xxxx     | 4 days 18:00:00
-		     running | 4364499 | 231055  | toolshed.g2.bx.psu.edu/repos/iuc/mothur_venn/mothur_venn/1.36.1.0         | xxxx     | 4 days 05:00:00
-		     running | 4366604 | 5183013 | toolshed.g2.bx.psu.edu/repos/iuc/dexseq/dexseq_count/1.24.0.0             | xxxx     | 3 days 20:00:00
-		     running | 4366605 | 5183016 | toolshed.g2.bx.psu.edu/repos/iuc/dexseq/dexseq_count/1.24.0.0             | xxxx     | 3 days 20:00:00
-		     queued  | 4350274 | 225743  | toolshed.g2.bx.psu.edu/repos/iuc/unicycler/unicycler/0.4.6.0              | xxxx     | 9 days 05:00:00
-		     queued  | 4353435 | 227038  | toolshed.g2.bx.psu.edu/repos/iuc/trinity/trinity/2.8.3                    | xxxx     | 8 days 08:00:00
-		     queued  | 4361914 | 229712  | toolshed.g2.bx.psu.edu/repos/iuc/unicycler/unicycler/0.4.6.0              | xxxx     | 5 days -01:00:00
-		     queued  | 4361812 | 229696  | toolshed.g2.bx.psu.edu/repos/iuc/unicycler/unicycler/0.4.6.0              | xxxx     | 5 days -01:00:00
-		     queued  | 4361939 | 229728  | toolshed.g2.bx.psu.edu/repos/nml/spades/spades/1.2                        | xxxx     | 4 days 21:00:00
-		     queued  | 4361941 | 229731  | toolshed.g2.bx.psu.edu/repos/nml/spades/spades/1.2                        | xxxx     | 4 days 21:00:00
+			$ gxadmin local query-queue
+			  state  |  id  | extid |                          tool_id                          | username | time_since_creation |       handler       | cores | mem  | partition | destination_id
+			---------+------+-------+-----------------------------------------------------------+----------+---------------------+---------------------+-------+------+-----------+-----------------
+			 running | 4385 | 4011  | upload1                                                   | cat      | 00:01:01.518932     | main.job-handlers.2 | 2     | 6144 |           | slurm
+			 queued  | 4387 | 4012  | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2 | cat      | 00:00:24.377336     | main.job-handlers.2 | 1     | 3072 |           | slurm
+			 queued  | 4388 | 4388  | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2 | cat      | 00:00:13.254505     | main.job-handlers.1 | 1     | 3072 |           | pulsar-nci-test
+			 queued  | 4389 | 4013  | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2 | cat      | 00:00:01.834048     | main.job-handlers.2 | 1     | 3072 |           | slurm
 	EOF
 
 	fields="count=9"
@@ -48,9 +41,9 @@ local_query-queue() { ##? [--all] [--seconds] [--since-update]: Detailed overvie
 			$username,
 			$nonpretty now() AT TIME ZONE 'UTC' - $time_column) as $time_column_name,
 			job.handler,
-            (REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'ntasks=(\d+)'))[1] as cores,
-            (REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'mem=(\d+)'))[1] as mem,
-            (REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'partition=(\d+)'))[1] as partition,
+			(REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'ntasks=(\d+)'))[1] as cores,
+			(REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'mem=(\d+)'))[1] as mem,
+			(REGEXP_MATCHES(encode(job.destination_params, 'escape'), 'partition=(\d+)'))[1] as partition,
 			COALESCE(job.destination_id, 'none') as destination_id
 		FROM job
 		FULL OUTER JOIN galaxy_user ON job.user_id = galaxy_user.id
---
+++ query-walltime-size-by-tool-with-info.sh
@@ -0,0 +1,50 @@
+local_query-walltime-size-by-tool-with-info() { ##? <tool> input tool substr,  # optional <limit>
+	tool_substr="$1"
+	[ ! "$2" ] && limit="10" || limit="$2"
+	handle_help "$@" <<-EOF
+
+    Produces a table with input size and running times for a given tool, provided as the first argument.
+    The argument is a substring of the tool_id e.g. trinity, trinity/2.9.1+galaxy1, Count1.  The second
+    argument is the number of rows to return (optional, default: 50).
+
+    gxadmin local query-walltime-size-by-tool busco 5
+     job_id  |          created           |          updated           |   username    |  state  |                          tool_id                           | runtime  | sum_input_size | destination
+    ---------+----------------------------+----------------------------+---------------+---------+------------------------------------------------------------+----------+----------------+--------------
+     7535204 | 2021-09-14 06:13:44.820678 | 2021-09-14 07:27:47.494749 | julia         | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.0.0+galaxy0 | 01:13:59 | 148 MB         | slurm_3slots
+     7533738 | 2021-09-13 14:10:02.462621 | 2021-09-13 18:06:15.366452 | paul          | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 03:56:10 | 31 MB          | slurm_1slot
+     7533781 | 2021-09-13 14:53:51.879839 | 2021-09-13 14:54:24.656188 | kevin         | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 00:00:31 | 2244 kB        | slurm_1slot
+     7533323 | 2021-09-13 08:15:12.673086 | 2021-09-13 12:10:33.847549 | paul          | ok      | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 | 03:55:18 | 31 MB          | slurm_1slot
+     7533213 | 2021-09-13 05:59:59.559287 | 2021-09-13 07:21:51.458181 | paul          | deleted | toolshed.g2.bx.psu.edu/repos/iuc/busco/busco/5.2.2+galaxy0 |          | 3970 MB        | slurm_3slots
+	EOF
+
+	read -r -d '' QUERY <<-EOF
+			SELECT
+				j.id as job_id,
+				j.create_time as created,
+				j.update_time as updated,
+				j.user_id as user_id,
+				j.state as state,
+				j.tool_id as tool_id,
+                (SELECT 
+                    TO_CHAR((jmn.metric_value || ' second')::interval, 'HH24:MI:SS')
+                    FROM job_metric_numeric jmn
+                    WHERE jmn.metric_name = 'runtime_seconds'
+                    AND jmn.job_id = j.id
+                ) as runtime,
+				(
+					SELECT
+					pg_size_pretty(SUM(d.total_size))
+					FROM dataset d, history_dataset_association hda, job_to_input_dataset jtid
+					WHERE hda.dataset_id = d.id
+					AND jtid.job_id = j.id
+					AND hda.id = jtid.dataset_id
+				) as sum_input_size,
+				j.destination_id as destination,
+                j.info as info
+			FROM job j
+			WHERE j.user_id = u.id
+			AND position('$tool_substr' in j.tool_id)>0
+			ORDER BY j.update_time desc
+			LIMIT $limit
+	EOF
+}
