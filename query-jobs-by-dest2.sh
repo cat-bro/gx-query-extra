86dcf304b02effaa8c656dbc1b1bb4bc4f5559df
cat-bro <cathjb@gmail.com>
2021-12-04 11:07:07 +1100

    .

--- query-jobs-by-dest2.sh
+++ query-jobs-by-dest2.sh
@@ -12,6 +12,8 @@ local_query-jobs-by-dest2() { ## input destination substr,  # optional limit
 				u.username,
 				j.state as state,
 				j.tool_id as tool_id,
+				(REGEXP_MATCHES(encode(j.destination_params, 'escape'), 'ntasks=(\d+)'))[1] as cores,
+				(REGEXP_MATCHES(encode(j.destination_params, 'escape'), 'mem=(\d+)'))[1] as mem,
 				j.destination_id as destination,
 				j.job_runner_external_id as external_id
 			FROM job j, galaxy_user u
