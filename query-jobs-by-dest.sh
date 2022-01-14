6666c5e411fc2675d4c3e80e21cf399dfdc16c22
cat-bro <cathjb@gmail.com>
2022-01-14 15:29:04 +1100

    .

--- query-jobs-by-dest.sh
+++ query-jobs-by-dest.sh
@@ -14,7 +14,7 @@ local_query-jobs-by-dest() { ## input destination substr,  # optional limit
 				j.tool_id as tool_id,
 				(REGEXP_MATCHES(encode(j.destination_params, 'escape'), 'ntasks=(\d+)'))[1] as cores,
 				(REGEXP_MATCHES(encode(j.destination_params, 'escape'), 'mem=(\d+)'))[1] as mem,
-				j.job_runner_external_id as external_id
+				j.destionation_id as destination_id
 			FROM job j
 			FULL OUTER JOIN galaxy_user u ON j.user_id = u.id
 			WHERE j.user_id = u.id
