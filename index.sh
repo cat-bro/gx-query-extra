b129ddda7771a5fd4e68a99f17a863b9de66f9d4
cat-bro <cathjb@gmail.com>
2022-03-18 14:34:29 +1100

    add traceback query

--- index.sh
+++ index.sh
@@ -15,6 +15,7 @@ source $HERE/query-time-size-by-tool.sh
 source $HERE/query-time-size-by-dest.sh
 source $HERE/query-jobs-tool-and-stderr.sh
 source $HERE/query-jobs-tool-and-stdout.sh
+source $HERE/query-jobs-tool-and-traceback.sh
 source $HERE/query-new-job-sizes.sh
 source $HERE/query-queued-job-sizes.sh
 source $HERE/query-job-input-datasets2.sh
---
+++ query-jobs-tool-and-traceback.sh
@@ -0,0 +1,36 @@
+local_query-jobs-tool-and-traceback() { ## input tool substr,  stderr substr, optional limit
+    tool_substr="$1"
+	tool_traceback_substr="$2"
+    limit="$3"
+	[ ! "$3" ] && limit="50" || limit="$3"
+	handle_help "$@" <<-EOF
+	EOF
+
+	read -r -d '' QUERY <<-EOF
+			SELECT
+				j.id as job_id,
+				j.create_time as created,
+				(
+					(SELECT MIN(jsh.create_time) FROM job_state_history jsh
+					WHERE jsh.job_id = j.id AND jsh.state in ('error', 'deleted', 'ok')) -
+					(SELECT MIN(jsh.create_time) FROM job_state_history jsh
+					WHERE jsh.job_id = j.id AND jsh.state = 'running')
+				) as run_time,
+				j.state as state,
+				j.tool_id as tool_id,
+				(
+					SELECT
+					pg_size_pretty(SUM(d.total_size))
+					FROM dataset d, history_dataset_association hda, job_to_input_dataset jtid
+					WHERE hda.dataset_id = d.id
+					AND jtid.job_id = j.id
+					AND hda.id = jtid.dataset_id
+				) as sum_input_size,
+				j.destination_id as destination
+			FROM job j
+			WHERE position('$tool_traceback_substr' in j.traceback)>0
+            AND position('$tool_substr' in j.tool_id)>0
+			ORDER BY j.create_time desc
+			LIMIT $limit
+	EOF
+}
