f7f292590975380f331149e7cfd4b300b9ab35ba
cat-bro <cathjb@gmail.com>
2022-10-25 13:36:38 +1100

    .

--- query-test-for-gxadmin.sh
+++ query-test-for-gxadmin.sh
@@ -1,3 +1,29 @@
+local_query-jobs() {  ## [--tool]
+	# tool_substr="$1"
+	# [ ! "$2" ] && limit="50" || limit="$2"
+	limit=50
+	handle_help "$@" <<-EOF
+	EOF
+
+	read -r -d '' QUERY <<-EOF
+			SELECT
+				j.id as job_id,
+				j.create_time as created,
+				j.update_time - j.create_time as duration,
+				u.username,
+				j.state as state,
+				j.tool_id as tool_id,
+				j.destination_id as destination,
+				j.job_runner_external_id as external_id
+			FROM job j, galaxy_user u
+			WHERE j.user_id = u.id
+			AND position('$args.tool' in j.tool_id)>0
+			ORDER BY j.create_time desc
+			LIMIT $limit
+	EOF
+}
+
+
 local_query-job-info() { ## <-|job_id [job_id [...]]> : Retrieve information about jobs given some job IDs
 	handle_help "$@" <<-EOF
 		Retrieves information on a job, like the host it ran on,
