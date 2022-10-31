748cff45f3c1bb01afdb8fcc5a4a956b4c090e77
cat-bro <cathjb@gmail.com>
2022-10-31 16:15:53 +1100

    .

--- query-test-for-gxadmin.sh
+++ query-test-for-gxadmin.sh
@@ -2,14 +2,27 @@ get_user_filter(){
 	echo "(galaxy_user.email = '$1' or galaxy_user.username = '$1' or galaxy_user.id = CAST(REGEXP_REPLACE('$1', '.*\D+.*', '-1') AS INTEGER))"
 }
 
-local_query-jobs() {  ## [--tool] [--limit]
+local_query-jobs() {  ## [--tool|-t] [--destination|d] [--limit|-l] [--states|-s] [--user|-u] [--terminal] [--nonterminal]
 	handle_help "$@" <<-EOF
+		Displays a list of jobs ordered from most recently updated, which can be
+		filtered by states, destination_id, tool_id or user. For example:
 
-	Displays a list of jobs ordered from most recently updated, which can be
-	filtered by state, destination_id, tool_id or user
-
-
-	
+		$ gxadmin query jobs --destination=pulsar-nci-test
+		  id   |     create_time     |     update_time     | user_id |  state  |                                           tool_id                                           |  handler  |         destination         | external_id
+		-------+---------------------+---------------------+---------+---------+---------------------------------------------------------------------------------------------+-----------+-----------------------------+-------------
+		 14701 | 2022-10-31 00:54:43 | 2022-10-31 00:55:02 |      16 | ok      | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2                                   | handler_0 | pulsar-nci-test             | 14701
+		 14700 | 2022-10-31 00:53:45 | 2022-10-31 00:54:04 |      16 | ok      | toolshed.g2.bx.psu.edu/repos/devteam/fastqc/fastqc/0.71                                     | handler_0 | pulsar-nci-test             | 14700
+		 14588 | 2022-10-19 10:45:42 | 2022-10-19 10:46:01 |      16 | ok      | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2                                   | handler_2 | pulsar-nci-test             | 14588
+		 14584 | 2022-10-19 10:45:12 | 2022-10-19 10:45:31 |      16 | ok      | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2                                   | handler_2 | pulsar-nci-test             | 14584
+		 14580 | 2022-10-19 10:44:43 | 2022-10-19 10:45:02 |      16 | ok      | toolshed.g2.bx.psu.edu/repos/devteam/bwa/bwa_mem/0.7.17.2                                   | handler_2 | pulsar-nci-test             | 14580
+			
+		$ gxadmin query jobs -d=pulsar-nci-test -t=bionano
+		  id   |     create_time     |     update_time     | user_id | state |                                        tool_id                                         |       handler       |         destination         | external_id
+		-------+---------------------+---------------------+---------+-------+----------------------------------------------------------------------------------------+---------------------+-----------------------------+-------------
+		 14085 | 2022-09-08 07:44:48 | 2022-09-08 08:21:58 |       3 | ok    | toolshed.g2.bx.psu.edu/repos/bgruening/bionano_scaffold/bionano_scaffold/3.6.1+galaxy3 | handler_2           | pulsar-nci-test             | 14085
+		 14080 | 2022-09-08 07:00:14 | 2022-09-08 07:44:31 |       3 | ok    | toolshed.g2.bx.psu.edu/repos/bgruening/bionano_scaffold/bionano_scaffold/3.6.1+galaxy3 | handler_0           | pulsar-nci-test             | 14080
+		 14076 | 2022-09-08 06:15:37 | 2022-09-08 06:59:59 |       3 | error | toolshed.g2.bx.psu.edu/repos/bgruening/bionano_scaffold/bionano_scaffold/3.6.1+galaxy3 | handler_2           | pulsar-nci-test             | 14076
+		 14071 | 2022-09-08 05:38:25 | 2022-09-08 06:15:22 |       3 | error | toolshed.g2.bx.psu.edu/repos/bgruening/bionano_scaffold/bionano_scaffold/3.6.1+galaxy3 | handler_1           | pulsar-nci-test             | 14071
 	EOF
 
 	tool_id_substr=''
@@ -60,7 +73,7 @@ local_query-jobs() {  ## [--tool] [--limit]
 
 	read -r -d '' QUERY <<-EOF
 			SELECT
-				job.id as id,
+				job.id as job_id,
 				job.create_time::timestamp(0) as create_time,
 				job.update_time::timestamp(0) as update_time,
 				job.user_id as user_id,
