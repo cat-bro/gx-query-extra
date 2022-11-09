3b7f50d29882e8e0866ac66efc637c772be72239
cat-bro <cathjb@gmail.com>
2022-11-09 14:10:10 +1100

    .

--- query-test-for-gxadmin.sh
+++ query-test-for-gxadmin.sh
@@ -3,7 +3,6 @@ get_user_filter(){
 }
 
 local_query-jobs()  { ##? [--tool] [--destination] [--limit] [--states] [--user] [--terminal] [--nonterminal]: List jobs ordered by most recently updated
-# local_query-jobs()  { ##? [--tool|-t] [--destination|d] [--limit|-l] [--states|-s] [--user|-u] [--terminal] [--nonterminal]: List jobs ordered by most recently updated
 	handle_help "$@" <<-EOF
 		Displays a list of jobs ordered from most recently updated, which can be filtered by states, destination_id,
 		tool_id or user. By default up to 50 rows are returned which can be adjusted with the --limit or -l flag.
