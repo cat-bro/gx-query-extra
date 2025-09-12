369fd6cc50385469a65cfa4f39590e492e13da31
cat-bro <cathjb@gmail.com>
2025-09-12 16:26:47 +1000

    .

--- query-user-info.sh
+++ query-user-info.sh
@@ -3,12 +3,25 @@ local_query-user-info() {
 
 	EOF
 
+	assert_count_ge $# 1 "No users specified"
+
+	if [[ "$1" == "-" ]]; then
+		# read users from stdin
+		users=$(cat | paste -s -d' ')
+	else
+		# read from $@
+		users=$*;
+	fi
+
+	# shellcheck disable=SC2068
+	users_string=$(join_by ',' ${users[@]})
+
 	read -r -d '' QUERY <<-EOF
 		SELECT
 			username, id, email, create_time AT TIME ZONE 'UTC' as create_time, active, deleted, purged, pg_size_pretty(disk_usage) as disk_usage
 		FROM
 			galaxy_user
 		WHERE
-			(galaxy_user.email = '$1' or galaxy_user.username = '$1' or galaxy_user.id = CAST(REGEXP_REPLACE(COALESCE('$1','0'), '[^0-9]+', '0', 'g') AS INTEGER))
+			(galaxy_user.email in (users_string) or galaxy_user.username in (users_string) or galaxy_user.id::string in (users_string))
 	EOF
 }
