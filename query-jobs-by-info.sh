49673d79f19f907cfabe4d7e5811527ec3e8ccfe
cat-bro <cathjb@gmail.com>
2025-12-13 06:29:50 +1100

    Update query-jobs-by-info.sh

--- query-jobs-by-info.sh
+++ query-jobs-by-info.sh
@@ -7,13 +7,13 @@ local_query-jobs-by-info() { ## input destination substr,  # optional limit
 	read -r -d '' QUERY <<-EOF
 			SELECT
 				j.id as job_id,
-				j.create_time as created,
+				j.update_time as updated,
 				j.state as state,
 				j.tool_id as tool_id,
 				j.destination_id as destination
 			FROM job j
 			WHERE position('$job_info_substr' in j.info)>0
-			ORDER BY j.create_time desc
+			ORDER BY j.update_time desc
 			LIMIT $limit
 	EOF
 }
