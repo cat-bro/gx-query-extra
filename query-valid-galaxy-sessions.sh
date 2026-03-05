4ae85957e35b3b9c25078523a33584907d789495
cat-bro <cathjb@gmail.com>
2026-03-05 13:01:21 +1100

    Update query-valid-galaxy-sessions.sh

--- query-valid-galaxy-sessions.sh
+++ query-valid-galaxy-sessions.sh
@@ -12,13 +12,13 @@ EOF
 				email,
 				remote_host,
 				remote_addr,
-				referer,
 				is_valid,
 				last_action
 			FROM galaxy_session
 			LEFT JOIN galaxy_user AS galaxy_session_user
 			ON galaxy_session.user_id = galaxy_session_user.id
 			WHERE galaxy_session.is_valid = 't'
+			AND galaxy_session.user IS NOT NULL
 			ORDER BY galaxy_session.create_time DESC
 EOF
 }
