fc387f28bf551f997e19ae4fdced17c1abb90f9c
cat-bro <cathjb@gmail.com>
2026-03-16 12:26:53 +1100

    Update query-history-exports.sh

--- query-history-exports.sh
+++ query-history-exports.sh
@@ -4,6 +4,13 @@ local_query-history-exports() { ##? <limit> : Show most recent history exports
 
 	Show most recent history export tasks with an optional argument for number of rows returned (default 50).
 
+	$ gxadmin local query-history-exports 3
+	  id  |     create_time     |            task_uuid             | history_id | user_id | total_size |                  result_data
+	------+---------------------+----------------------------------+------------+---------+------------+-----------------------------------------------
+	 3275 | 2026-03-15 10:27:25 | 5557cfe535c34c959baa7e7fa4ecddc6 |    2800101 |   12345 | 149 GB     | {"uri": null, "error": null, "success": true}
+	 3274 | 2026-03-14 05:13:12 | 555611b2b02d4ec59a9246b73bbdda15 |    2840202 |   42424 | 11 GB      | {"uri": null, "error": null, "success": true}
+	 3273 | 2026-03-14 04:08:40 | 555c6a5a7eb44836a1c8a705bcfa7714 |    2790303 |   23456 | 279 GB     | {"uri": null, "error": null, "success": true}
+	(3 rows)
 
 	EOF
 
